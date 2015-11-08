4c9284ba1d2495b0fa9c41e7cd7d14de80fb9236
cubuspl42 <cubuspl42@stgn.pl>
2015-11-08 14:11:21 +0100

    Dart

--- WapUniverse.ts
+++
@@ -1,3 +0,0 @@
-$(document).ready(() => {
-  document.write("Hello, Universe!")
-});
--- dist/WapUniverse.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    document.write("Hello, Universe!");
-});
-//# sourceMappingURL=WapUniverse.js.map
