073a6c5da8de74a5f70dd2aac7a1682b1089d28b
Jakub Trzebiatowski <cubuspl42@gmail.com>
2016-05-23 19:08:47 +0200

    Add makeMatrix function

--- wapuniverse/src/main/kotlin/io/github/wapuniverse/utils/Matrix.kt
+++ wapuniverse/src/main/kotlin/io/github/wapuniverse/utils/Matrix.kt
@@ -22,3 +22,13 @@ class Matrix<out T>(private val rows: List<List<T>> = emptyList<List<T>>()) {
 
     fun getElement(i: Int, j: Int): T = rows[i][j]
 }
+
+fun <T> makeMatrix(m: Int, n: Int, vararg  elements: T): Matrix<T> {
+    assert(elements.size == m * n)
+    val rows = mutableListOf<List<T>>()
+    for (i in 0..m-1) {
+        val row = elements.slice(i*n..(i+1)*n)
+        rows.add(row)
+    }
+    return Matrix(rows)
+}
