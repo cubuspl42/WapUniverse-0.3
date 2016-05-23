fa767b9245710439c164779faa16b0fd0cc5638a
Jakub Trzebiatowski <cubuspl42@gmail.com>
2016-05-23 18:38:01 +0200

    Add ImageMap class and ImageLoadingUtils

--- wapuniverse/src/main/kotlin/io/github/wapuniverse/controller/MainController.kt
+++ wapuniverse/src/main/kotlin/io/github/wapuniverse/controller/MainController.kt
@@ -2,17 +2,17 @@ package io.github.wapuniverse.controller
 
 import io.github.wapuniverse.editor.loadImageSetDatabaseFromFile
 import io.github.wapuniverse.view.SceneView
+import io.github.wapuniverse.view.loadImageMapFromPath
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.canvas.Canvas
-import javafx.scene.image.Image
 import javafx.scene.layout.Region
 import javafx.scene.paint.Color
 import java.net.URL
 import java.util.*
 
 
-private val IMAGE_PATH = "file:/home/kuba/tmp/CLAW/CLAW/IMAGES/FRAME059.png"
+private val IMAGE_BASE_DIR_PATH = "/home/kuba/tmp/CLAW/"
 
 class MainController : Initializable {
     @FXML
@@ -25,12 +25,14 @@ class MainController : Initializable {
 
     private val imageSetDatabase = loadImageSetDatabaseFromFile("imageSetDatabase.yaml")
 
+    private val imageMap = loadImageMapFromPath(imageSetDatabase, IMAGE_BASE_DIR_PATH)
+
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         sceneCanvas.widthProperty().bind(root.widthProperty())
         sceneCanvas.heightProperty().bind(root.heightProperty())
         sceneCanvas.isFocusTraversable = true
 
-        val image = Image(IMAGE_PATH)
+        val image = imageMap.findObjectImage(1, "LEVEL_OFFICER", -1)!!
         sceneView = SceneView(sceneCanvas, image)
         sceneView.render()
 
---
+++ wapuniverse/src/main/kotlin/io/github/wapuniverse/view/ImageMap.kt
@@ -0,0 +1,34 @@
+package io.github.wapuniverse.view
+
+import io.github.wapuniverse.editor.ImageSetDatabase
+import javafx.scene.image.Image
+
+
+private val pidExt = "PID"
+private val pngExt = "png"
+
+class ImageMap(
+        private val imageSetDatabase: ImageSetDatabase,
+        private val rawImageMap: Map<String, Image>
+) {
+    private fun findImage(pidImagePath: String): Image? {
+        val pngImagePath = pidImagePath.replace(pidExt, pngExt)
+        val image = rawImageMap[pngImagePath]
+        return image
+    }
+
+    fun findObjectImage(levelIndex: Int, imageSetId: String, frameIndex: Int): Image? {
+        val imageMd = imageSetDatabase.findObjectImageMetadata(levelIndex, imageSetId, frameIndex)
+        return findImage(imageMd.path)
+    }
+
+    fun findTileImage(levelIndex: Int, tileImageSetName: String, tileIndex: Int): Image? {
+        val tileMd = imageSetDatabase.findTileMetadata(levelIndex, tileImageSetName, tileIndex)
+        return findImage(tileMd.path)
+    }
+}
+
+fun loadImageMapFromPath(imageSetDatabase: ImageSetDatabase, baseDirPath: String): ImageMap {
+    val rawImageMap = loadAllImagesRecursively(baseDirPath, "png")
+    return ImageMap(imageSetDatabase, rawImageMap)
+}
