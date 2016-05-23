e0765e9294c95a9c6e2cf6777261aad71bf90b78
Jakub Trzebiatowski <cubuspl42@gmail.com>
2016-05-23 19:44:24 +0200

    Add AlphaTileMapper

---
+++ wapuniverse/src/main/kotlin/io/github/wapuniverse/editor/AlphaTileMapper.kt
@@ -0,0 +1,33 @@
+package io.github.wapuniverse.editor
+
+import io.github.wapuniverse.lsd.FormulaGroup
+import java.util.*
+
+
+class AlphaTileMapper(formulaGroup: FormulaGroup) {
+    val formulaMap = HashMap<Set<Int>, Int>()
+
+    init {
+        formulaGroup.formulas.forEach {
+            formulaMap[it.alphaTileSet] = it.tileIndex
+        }
+    }
+
+    fun mapAlphaTileSet(set: Set<AlphaTile>): Int? {
+        val stack = set.toList()
+                .sortedBy { it.z }
+                .map { it.ordinal }
+        for (i in 0..stack.size - 1) {
+            val subSet = stack.subList(i, stack.size).toSet()
+            val tileIndex = formulaMap[subSet]
+            if (tileIndex != null) {
+                return tileIndex
+            }
+        }
+        return null
+    }
+}
+
+
+
+
