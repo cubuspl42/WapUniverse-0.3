ef23647722bed83a7543f4a48ed53132c3a712bc
Jakub Trzebiatowski <cubuspl42@gmail.com>
2016-06-03 17:17:07 +0200

    Add tile caching

--- wapuniverse/src/main/kotlin/io/github/wapuniverse/editor/TileLayer.kt
+++ wapuniverse/src/main/kotlin/io/github/wapuniverse/editor/TileLayer.kt
@@ -10,12 +10,23 @@ import java.util.*
 val tileWidth = 64.0
 
 class AlphaTileMatrix {
+    internal var _tileLayer: TileLayer? = null
+
     var offset = Vec2i()
+        set(value) {
+            _tileLayer?._updateCache()
+            field = value
+        }
 
     var alphaTiles = Matrix<AlphaTile>()
+        set(value) {
+            _tileLayer?._updateCache()
+            field = value
+        }
 
     val rect: Rectangle2Di
         get() = Rectangle2Di(offset.x, offset.y, alphaTiles.width, alphaTiles.height)
+
 
     fun getAlphaTile(ai: Int, aj: Int): AlphaTile {
         val ri = ai - offset.y
@@ -31,24 +42,44 @@ class TileLayer(
         val imageSet: String
 ) {
     private val matrices = HashSet<AlphaTileMatrix>()
+    private val tileCache = HashMap<Vec2i, Int>()
 
     fun addMatrix(matrix: AlphaTileMatrix) {
+        assert(matrix._tileLayer == null)
+        matrix._tileLayer = this
         matrices.add(matrix)
         matrixAdded._emit(matrix)
+        _updateCache()
     }
 
     val matrixAdded = Signal<AlphaTileMatrix>()
 
     fun getTile(i: Int, j: Int): Int {
+        return tileCache[Vec2i(j, i)] ?: DEFAULT_TILE_INDEX
+    }
+
+    private fun cacheTile(p: Vec2i) {
         val alphaTileSet = matrices
-            .filter { it.rect.contains(j, i) }
-            .map { it.getAlphaTile(i, j) }
-            .toSet()
+                .filter { it.rect.contains(p.x, p.y) }
+                .map { it.getAlphaTile(p.y, p.x) }
+                .toSet()
         val tileIndex = alphaTileMapper.mapAlphaTileSet(alphaTileSet)
         if (tileIndex != null) {
-            return tileIndex
-        } else {
-            return DEFAULT_TILE_INDEX
+            tileCache[p] = tileIndex
         }
+    }
+
+    internal fun _updateCache() {
+        tileCache.clear()
+        matrices
+                .forEach {
+                    val r = it.rect
+                    for (i in 0..r.height - 1) {
+                        for (j in 0..r.width - 1) {
+                            val p = it.offset + Vec2i(j, i)
+                            cacheTile(p)
+                        }
+                    }
+                }
     }
 }
